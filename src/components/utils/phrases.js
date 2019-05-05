155c3342409b85585355b8dc1c6e743f9b06660b
Daniel Marchi <marchi775@gmail.com>
2019-05-05 06:32:53 -0300

    random funny phrases

---
+++ src/components/utils/phrases.js
@@ -0,0 +1,21 @@
+export const workPhrases = [
+    'The elevator to success is out of order. You’ll have to use the stairs, one step at a time.',
+    'Think like a proton. Always positive.',
+    'People say nothing is impossible, but I do nothing every day.',
+    'Life is hard, like javascript',
+    'fetch(\'https://www.reddit.com/r/ProgrammerHumor/\').then(function(response) { return response.json();}).then(function(myJson) {()=> JSON.stringify(myJson)));});',
+    'Life is like a sewer… what you get out of it depends on what you put into it.',
+    'Damn, looks like we have a  bug here...tap start, we gotta fix that!',
+    'Work work work!'
+
+]
+
+export const breakPhrases = [
+    'Try to pause each day and take a walk to view nature.',
+    'When’s the last time you disconnected and took a vacation?',
+    'Rest will make you do things better.',
+    'Just go play some games',
+    'Hey! you fixed that bug!',
+    'Eat vegetables! With bacon of course!'
+
+]
