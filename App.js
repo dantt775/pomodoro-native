3d218c318dc8a9a8c592f4cdbe3b758e8e49af71
Daniel Marchi <marchi775@gmail.com>
2019-05-10 01:06:26 -0300

    passing function that changes the backgroundColor of parent by child

--- App.js
+++ App.js
@@ -5,22 +5,37 @@ import { StyleSheet, Text, View } from 'react-native';
 import Timer from './src/components/Timer'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+}
+  state = {
+    color: '#E98C8C',
+  }
+
+  handleColorChange = (newColor) => {
+    console.log('handle Color change chamado')
+    this.setState({
+      color: newColor
+    })
+    return newColor;
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Timer />
+      <View style={[styles.container, { backgroundColor: this.state.color }]}>
+        <Timer parentColor={this.handleColorChange}/>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-      flex: 2,
-      justifyContent: 'center',
-      alignItems: 'center',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  timer: {        
-      fontSize: 40,
+  timer: {
+    fontSize: 40,
   }
 });
